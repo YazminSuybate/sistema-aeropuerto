38582bd2caf8640029d48f810b2242633d71a15b
YazminSuybate <yaaazscx@gmail.com>
2025-11-12 12:33:12 -0500

    Migracion duplicada

--- backend-api/prisma/migrations/20251109194928_unique_pasjero_documento/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[documento_id]` on the table `pasajero` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `pasajero_documento_id_key` ON `pasajero`(`documento_id`);
